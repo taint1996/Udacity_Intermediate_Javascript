13672091e41f42f19739178f7d48c361297e3193
Tai Nguyen-Trung <tai.nguyentrung1996@gmail.com>
2024-04-14 22:48:26 +0700

    Add random fact - human Title and Bird title

--- Dinosaurs/app.js
+++ Dinosaurs/app.js
@@ -16,17 +16,30 @@ fetch('./dino.json')
         });
       }
 
+      getRamdomFact() {
+        const facts = [
+          this.fact,
+          `The ${this.species} lived in the ${this.where} Pediod`,
+          `The ${this.species} was a ${this.diet} eater`,
+          `The ${this.species} lived during the ${this.when}`,
+          this.compareDiet(),
+          this.compareHeight(),
+          this.compareWeight()
+        ];
+        return facts[Math.floor(Math.random() * facts.length)];
+      }
+
       // Create Dino Compare Method 1
       // NOTE: Weight in JSON file is in lbs, height in inches.
       compareWeight(humanWeight) {
-        return `${this.species} is ${
+        return `The ${this.species} is ${
           this.weight > humanWeight ? 'bigger' : 'smaller'
         } than you`;
       }
       // Create Dino Compare Method 2
       // NOTE: Height in JSON file is in lbs, height in inches.
       compareHeight(humanHeight) {
-        return `${this.species} is ${
+        return `The ${this.species} is ${
           this.height > humanHeight ? 'taller' : 'shorter'
         } than you`;
       }
@@ -34,7 +47,7 @@ fetch('./dino.json')
       // Create Dino Compare Method 3
       // NOTE: Diet in JSON file is in lbs, height in inches.
       compareDiet(humanDiet) {
-        return `Wow!. You and ${this.species} are ${
+        return `Wow!. You and the ${this.species} are ${
           this.diet === humanDiet ? 'same' : 'different'
         } diet.`;
       }
@@ -53,6 +66,7 @@ fetch('./dino.json')
       weight: 0,
       height: 0,
       inches: 0,
+      diet: '',
       setName: function (inpName) {
         this.name = inpName;
       },
@@ -62,8 +76,11 @@ fetch('./dino.json')
       setHeight: function (inpHeight) {
         this.height = inpHeight;
       },
-      setInches: function (inpInch) {
-        this.inches = inpInch;
+      setDiet: function (inpDiet) {
+        this.diet = inpDiet;
+      },
+      setInches: function (inpInches) {
+        this.inches = inpInches;
       }
     };
 
@@ -73,37 +90,54 @@ fetch('./dino.json')
       const titles = dinos.map((dino) => {
         const tile = document.createElement('div');
         tile.classList.add('grid-item');
+
         tile.innerHTML = `
-            <h3>${dino.species}</h3>
-            <p>${dino.fact}</p>
-            <p>${dino.compareWeight(Human.weight)}</p>
-            <p>${dino.compareHeight(Human.height)}</p>
-            <p>${dino.compareDiet(Human.diet)}</p>
+            <h3>The ${dino.species}</h3>
+            <img src="./images/${dino.species.toString().toLowerCase()}.png" />
+            <p>${dino.getRamdomFact()}</p>
         `;
         return tile;
       });
+
+      // Add tiles to DOM
+      // Add Human Title
+      const createHumanTitle = document.createElement('div');
+      createHumanTitle.classList.add('grid-item');
+      createHumanTitle.innerHTML = `
+    <h3>${Human.name}</h3>
+    <img src="./images/human.png" alt="Human">
+    <p>No facts available for humans
+    </p>`;
+      titles.splice(4, 0, createHumanTitle);
+
       grid.append(...titles);
     };
 
-    // Add tiles to DOM
     // Remove form from screen
     const removeForm = () => {
       const form = document.getElementById('dino-compare');
       form.style.display = 'none';
     };
 
+    const setHumanData = () => {
+      const humanName = document.getElementById('name').value;
+      const feet = parseInt(document.getElementById('height').value);
+      const inches = parseInt(document.getElementById('inches').value);
+      const humanHeight = feet * 12 + inches;
+      const humanWeight = parseInt(document.getElementById('weight').value);
+      const humanDiet = document.getElementById('diet').value;
+
+      Human.setName(humanName);
+      Human.setWeight(humanWeight);
+      Human.setHeight(humanHeight);
+      Human.setDiet(humanDiet);
+    };
+
     // On button click, prepare and display infographic
     document
       .getElementById('btn-compare')
       .addEventListener('click', function () {
-        const humanName = document.getElementById('name').value;
-        const humanWeight = parseInt(document.getElementById('weight').value);
-        const humanHeight = parseInt(document.getElementById('height').value);
-
-        Human.setName(humanName);
-        Human.setWeight(humanWeight);
-        Human.setHeight(humanHeight);
-
+        setHumanData();
         removeForm();
         generateTitles();
       });
